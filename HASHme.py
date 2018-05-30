3f0ed4e38cd98f0be19181c9787beec5aa98bd5e
root <root@localhost.localdomain>
2018-05-30 18:44:57 +0000

    Fix

--- HASHme.py
+++ HASHme.py
@@ -25,40 +25,40 @@ def HASHme(fname,hashname):
        print("\n"+cor[0]+"["+cor[2]+"!"+cor[0]+"][ERROR] "+cor[2]+"! File["+cor[1]+fname+cor[2]+"] Is Not Found "+cor[0]+"!!!")
        print(cor[0]+"["+cor[2]+"!"+cor[0]+"]"+cor[1]+" Please Check Your File Location "+cor[4]+"And Try Again "+cor[1]+":)")
        exit(1)
- hashs = ["md5","sha1","sha224","sha256","sha384","sha512"]
+ hashs = ["md5","MD5","sha1","SHA1","sha224","SHA224","sha256","SHA256","sha384","SHA384","sha512","SHA512"]
  if hashname in hashs:
   try:
-    if hashname =="md5":
+    if hashname =="md5" or hashname == "MD5":
      hash_md5 = hashlib.md5()
      with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
      return hash_md5.hexdigest()
-    elif hashname =="sha1":
+    elif hashname =="sha1" or hashname == "SHA1":
      hash_sha1 = hashlib.sha1()
      with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_sha1.update(chunk)
      return hash_sha1.hexdigest()
-    elif hashname =="sha224":
+    elif hashname =="sha224" or hashname == "SHA224":
      hash_sha224 = hashlib.sha224()
      with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_sha224.update(chunk)
      return hash_sha224.hexdigest()
-    elif hashname =="sha256":
+    elif hashname =="sha256" or hashname == "SHA256":
      hash_sha256 = hashlib.sha256()
      with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_sha256.update(chunk)
      return hash_sha256.hexdigest()
-    elif hashname =="sha384":
+    elif hashname =="sha384" or hashname == "SHA384":
      hash_sha384 = hashlib.sha384()
      with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_sha384.update(chunk)
      return hash_sha384.hexdigest()
-    elif hashname =="sha512":
+    elif hashname =="sha512" or hashname == "SHA512":
      hash_sha512 = hashlib.sha512()
      with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
@@ -100,13 +100,13 @@ python HASHme.py -H sha256 -F /root/Desktop/file.txt
 python HASHme.py -H sha384 -F /root/Desktop/file.txt
 python HASHme.py -H sha512 -F /root/Desktop/file.txt
 
-""")
+""",version="1.0")
 ## Main Function
 #--==--==--==--#
 def main():
  # Add Options&args
  parse.add_option("-H","--hashsum",dest="hashname",type="string")
- parse.add_option("-F","--file",dest="filename",type="string")
+ parse.add_option("-F","-f","--file",dest="filename",type="string")
  (options,args) = parse.parse_args()
 
  if options.hashname !=None and options.filename !=None:
